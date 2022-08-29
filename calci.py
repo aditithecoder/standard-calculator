951e69b17f81c30220a3f84aab7726c33c923cfa
akash19here <spacex3nasa@gmail.com>
2022-08-29 23:21:14 +0530

    Made some logical changes

--- calci.py
+++ calci.py
@@ -1,17 +1,20 @@
-print("\t\t\t\twelcome to my calculator")
-print("what you are looking for\n1)addition(+)\n2)subtraction(-)\n3)multiplication(*)\n4)division(/)")
-a=int(input("enter first number"))
-b=int(input("enter second number"))
-num=int(input("enter your choice number"))
-def calci(c,d):
+print("\t\t\t\tWelcome to my Calculator !")
+print("What you are looking for\n1)Addition(+)\n2)Subtraction(-)\n3)Multiplication(*)\n4)Division(/)")
+num=int(input("Enter the operation to Perform:"))
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+def calci(a,b):
     if (num==1):
-        print(c+d)
+        print("Addition is " + str(a+b))
     elif(num==2):
-        print(c-d)
+        print("Subtraction is " + str(a-b))
     elif(num==3):
-        print(c*d)
+        print("Multiplication is " + str(a*b))
     elif(num==4):
-        print(c/d)
+        if(b == 0):
+            print("Mathmatical Error")    
+        else:
+            print("Division is "+ str(a/b))
     else:
-        print("typing error")
+        print("Typing Error")
 calci(a,b)
